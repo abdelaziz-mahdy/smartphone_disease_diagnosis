cd07b2e9c25f93d256142432088a18cd09c38302
Abdelaziz Mahdy <zezohassam@gmail.com>
2022-06-17 23:16:47 +0200

    added models / main code for testing the models

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,254 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
-void main() {
-  runApp(const MyApp());
+import 'package:flutter/services.dart';
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
+import 'package:pytorch_lite/pigeon.dart';
+import 'package:pytorch_lite/pytorch_lite.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
+class _MyAppState extends State<MyApp> {
+  ClassificationModel? _imageModel;
+  //CustomModel? _customModel;
+  late ModelObjectDetection _objectModel;
+  String? _imagePrediction;
+  double? probability;
+  List? _prediction;
+  File? _image;
+  ImagePicker _picker = ImagePicker();
+  bool objectDetection = false;
+  List<ResultObjectDetection?> objDetect = [];
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  void initState() {
+    super.initState();
+    loadModel();
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  //load your model
+  Future loadModel() async {
+    String pathImageModel = "assets/models/yolo_x_Skin.torchscript";
+    //String pathCustomModel = "assets/models/custom_model.ptl";
+    String pathObjectDetectionModel =
+        "assets/models/Dental_cleaned_labels_best.torchscript";
+    try {
+      _imageModel = await PytorchLite.loadClassificationModel(
+          pathImageModel, 512, 512,
+          labelPath: "assets/labels/skin_model_(labels_cleaned)_yolox .txt");
+      //_customModel = await PytorchLite.loadCustomModel(pathCustomModel);
+      _objectModel = await PytorchLite.loadObjectDetectionModel(
+          pathObjectDetectionModel, 14, 640, 640,
+          labelPath: "assets/labels/labelmap_dental_cleaned_labels.txt");
+    } on PlatformException {
+      print("only supported for android");
+    }
+  }
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+  //run an image model
+  Future runObjectDetectionWithoutLabels() async {
+    //pick a random image
+    final PickedFile? image =
+        await _picker.getImage(source: ImageSource.gallery);
+    objDetect = await _objectModel
+        .getImagePredictionList(await File(image!.path).readAsBytes());
+    for (var element in objDetect) {
+      print({
+        "score": element?.score,
+        "className": element?.className,
+        "class": element?.classIndex,
+        "rect": {
+          "left": element?.rect.left,
+          "top": element?.rect.top,
+          "width": element?.rect.width,
+          "height": element?.rect.height,
+          "right": element?.rect.right,
+          "bottom": element?.rect.bottom,
+        },
+      });
+    }
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      //this.objDetect = objDetect;
+      _image = File(image.path);
     });
   }
 
+  Future runObjectDetection() async {
+    //pick a random image
+    final PickedFile? image =
+        await _picker.getImage(source: ImageSource.gallery);
+    objDetect = await _objectModel.getImagePrediction(
+        await File(image!.path).readAsBytes(),
+        minimumScore: 0.1,
+        IOUThershold: 0.3);
+    objDetect.forEach((element) {
+      print({
+        "score": element?.score,
+        "className": element?.className,
+        "class": element?.classIndex,
+        "rect": {
+          "left": element?.rect.left,
+          "top": element?.rect.top,
+          "width": element?.rect.width,
+          "height": element?.rect.height,
+          "right": element?.rect.right,
+          "bottom": element?.rect.bottom,
+        },
+      });
+    });
+    setState(() {
+      //this.objDetect = objDetect;
+      _image = File(image.path);
+    });
+  }
+
+  Future runClassification() async {
+    objDetect = [];
+    //pick a random image
+    final PickedFile? image =
+        await _picker.getImage(source: ImageSource.gallery);
+    //get prediction
+    //labels are 1000 random english words for show purposes
+/*
+    _imagePrediction = await _imageModel!
+        .getImagePrediction(await File(image!.path).readAsBytes());
+
+    List<double?>? predictionList = await _imageModel!.getImagePredictionList(
+      await File(image.path).readAsBytes(),
+    );
+
+    print(predictionList);
+    */
+    List<double?>? predictionListProbabilites =
+        await _imageModel!.getImagePredictionListProbabilities(
+      await File(image!.path).readAsBytes(),
+    );
+    //Gettting the highest Probability
+    double maxScoreProbability = double.negativeInfinity;
+    double sumOfProbabilites = 0;
+    int index = 0;
+    for (int i = 0; i < predictionListProbabilites!.length; i++) {
+      if (predictionListProbabilites[i]! > maxScoreProbability) {
+        maxScoreProbability = predictionListProbabilites[i]!;
+        sumOfProbabilites = sumOfProbabilites + predictionListProbabilites[i]!;
+        index = i;
+      }
+    }
+    print(predictionListProbabilites);
+    print(index);
+    print(sumOfProbabilites);
+    print(maxScoreProbability);
+
+    setState(() {
+      //this.objDetect = objDetect;
+      _image = File(image.path);
+      _imagePrediction = _imageModel?.labels[index];
+      probability = maxScoreProbability * 100;
+    });
+  }
+
+/*
+  //run a custom model with number inputs
+  Future runCustomModel() async {
+    _prediction = await _customModel!
+        .getPrediction([1, 2, 3, 4], [1, 2, 2], DType.float32);
+
+    setState(() {});
+  }
+*/
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Pytorch Mobile Example'),
+        ),
+        body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Expanded(
+              child: objDetect.isNotEmpty
+                  ? _image == null
+                      ? const Text('No image selected.')
+                      : _objectModel.renderBoxesOnImage(_image!, objDetect)
+                  : _image == null
+                      ? const Text('No image selected.')
+                      : Image.file(_image!),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Center(
+              child: Visibility(
+                visible: _imagePrediction != null,
+                child: Text(
+                    "$_imagePrediction , with confidence:${probability?.toStringAsFixed(2)} %"),
+              ),
             ),
+            /*
+            Center(
+              child: TextButton(
+                onPressed: runImageModel,
+                child: Row(
+                  children: [
+
+                    Icon(
+                      Icons.add_a_photo,
+                      color: Colors.grey,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            */
+
+            TextButton(
+              onPressed: runClassification,
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.blue,
+              ),
+              child: const Text(
+                "Run Classification",
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            TextButton(
+              onPressed: runObjectDetection,
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.blue,
+              ),
+              child: const Text(
+                "Run object detection with labels",
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            TextButton(
+              onPressed: runObjectDetectionWithoutLabels,
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.blue,
+              ),
+              child: const Text(
+                "Run object detection without labels",
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            Center(
+              child: Visibility(
+                visible: _prediction != null,
+                child: Text(_prediction != null ? "${_prediction![0]}" : ""),
+              ),
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
